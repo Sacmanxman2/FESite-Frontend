aac9be28c612ef7245834c363b055d53b192af03
Jared Sackett <Jake850@gmail.com>
2018-08-06 18:49:59 -0500

    Updated nodeJS Authentication, about to test for Heroku app

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const path = require('path');
 const serveStatic = require('serve-static');
+const basicAuth = require('express-basic-auth');
 
 let app = express();
+
+app.use(basicAuth({
+  users: { 'feadmin': 'Faithfu1Env0y!' },
+  challenge: true,
+  realm: 'Imb4T3st4pp',
+}));
 app.use(serveStatic(__dirname + "/dist"));
 
 const port = process.env.PORT || 5000;
