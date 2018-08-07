3e63d799fc3284849a5d995e74f7a18d111b31e6
Jared Sackett <Jake850@gmail.com>
2018-08-07 17:34:16 -0500

    Fixed HTML5 History Mode

--- server.js
+++ server.js
@@ -2,18 +2,25 @@ const express = require('express');
 const path = require('path');
 const serveStatic = require('serve-static');
 const basicAuth = require('express-basic-auth');
+const history = require('connect-history-api-fallback');
 
 let app = express();
+
+var hist = history({
+  rewrites: [
+    { from: /\*/, to: '/index.html' }
+  ]
+});
 
 app.use(basicAuth({
   users: { 'feadmin': 'Faithfu1Env0y!' },
   challenge: true,
   realm: 'Imb4T3st4pp',
 }));
-app.use(serveStatic(__dirname + "/dist"));
+app.use(hist);
+app.use(serveStatic(__dirname + '/dist'));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log('Listening on port ' + port)
 });
-
--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ const Contact = () => import('@/components/Contact')
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
